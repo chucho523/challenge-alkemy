30829c83bbfe607bf13567e29c27f455f4c9ed5f
Ruben Gianelli <rubengianelli8@gmail.com>
2021-09-01 13:18:02 -0300

    styles added to registration form

--- app-wallet/src/components/common/Register/index.js
+++ app-wallet/src/components/common/Register/index.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react'
 import register from '../../../services/register';
 import {Formik} from 'formik'; 
+import './styles.scss';
 
 const Register = () => {
     return (
@@ -15,32 +16,23 @@ const Register = () => {
                 onSubmit={(values, {setFieldError}) => {
                    return register(values)
                     .catch((e) => {
-                        const dataError = e.response.data.error;
-                        setFieldError(dataError.path, dataError.message)
+                        const dataError = e.response.data.error;//get error
+                        setFieldError(dataError.path, dataError.message)//set error
                     })
                 }}
             >
                 {
                     ({errors, handleSubmit, handleChange, isSubmitting}) => 
-                        <form onSubmit={handleSubmit}>
-                            <label>
-                                Email: 
-                                <input name='email' type="text" onChange={handleChange}></input>
-                            </label>
-                            <label>
-                                Name: 
-                                <input name='name' type="text" onChange={handleChange}></input>
-                            </label>
-                            <label>
-                                Password: 
-                                <input name='password' type="password" onChange={handleChange}></input>
-                            </label>
+                        <form onSubmit={handleSubmit} className="registerForm">
+                            <h4>Registration Form</h4>
+                            <input className="controls" placeholder="Enter your email" name='email' type="text" onChange={handleChange}></input>
+                            <input className="controls" placeholder="Enter your name" name='name' type="text" onChange={handleChange}></input>
+                            <input className="controls" placeholder="Enter your password" name='password' type="password" onChange={handleChange}></input>
                             <button type="submit" disabled={isSubmitting}>Sign In</button>
-                            <span>
-                                {errors.email || errors.password || errors.name || ''}
-                            </span>
-                        </form>
-                    
+                            <p>
+                                {errors.email || errors.password || errors.name || '' /*SHOW ERRORS */} 
+                            </p>
+                        </form>                   
                 }
             </Formik>
         </Fragment>
